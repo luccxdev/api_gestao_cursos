89f54603f7f29a6a2a86c80dd8465ece4637699b
luccxdev <luccasxdev@gmail.com>
2026-01-19 19:00:53 -0300

    Migrar resource/curso.py para usar SQLAlchemy (Parte 3)

--- resource/curso.py
+++ resource/curso.py
@@ -1,49 +1,113 @@
+# resource/curso.py
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from schemas.curso import CursoSchema, CursoSchemaUpdate
 
-blp = Blueprint('cursos', __name__, url_prefix='/cursos', description='Operações relacionadas a cursos')
+from db import db
+from models import CursoModel
+from schemas.curso import CursoSchema, CursoUpdateSchema
 
-# Dicionário para armazenar os cursos (em memória)
-cursos = {}
+blp = Blueprint("Cursos", "cursos", description="Operações com cursos")
 
-@blp.route('/')
-class ListaCursos(MethodView):
+
+@blp.route("/cursos")
+class CursosResource(MethodView):
     @blp.response(200, CursoSchema(many=True))
     def get(self):
-        """Lista todos os cursos"""
-        return list(cursos.values())
+        cursos = CursoModel.query.all()
+        return cursos
 
     @blp.arguments(CursoSchema)
     @blp.response(201, CursoSchema)
-    def post(self, novo_curso):
-        """Cria um novo curso"""
-        novo_id = max(cursos.keys(), default=0) + 1
-        novo_curso['id'] = novo_id
-        cursos[novo_id] = novo_curso
-        return novo_curso
+    def post(self, data):
+        if CursoModel.query.filter_by(nome=data["nome"]).first():
+            abort(409, message="Curso com esse nome já existe.")
 
-@blp.route('/<int:curso_id>')
-class DetalheCurso(MethodView):
+        curso = CursoModel(**data)
+
+        try:
+            db.session.add(curso)
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+            abort(500, message="Erro ao salvar curso no banco de dados.")
+
+        return curso
+
+
+@blp.route("/cursos/<int:curso_id>")
+class CursoResource(MethodView):
     @blp.response(200, CursoSchema)
     def get(self, curso_id):
-        """Obtém um curso por ID"""
-        if curso_id not in cursos:
-            abort(404, message=f'Curso {curso_id} não encontrado')
-        return cursos[curso_id]
+        curso = CursoModel.query.get(curso_id)
+        if not curso:
+            abort(404, message="Curso não encontrado.")
+        return curso
 
-    @blp.arguments(CursoSchemaUpdate)
+    @blp.arguments(CursoUpdateSchema)
     @blp.response(200, CursoSchema)
-    def put(self, dados_atualizacao, curso_id):
-        """Atualiza um curso"""
-        if curso_id not in cursos:
-            abort(404, message=f'Curso {curso_id} não encontrado')
-        cursos[curso_id].update(dados_atualizacao)
-        return cursos[curso_id]
+    def put(self, data, curso_id):
+        curso = CursoModel.query.get(curso_id)
+        if not curso:
+            abort(404, message="Curso não encontrado.")
 
-    @blp.response(204)
+        if "nome" not in data or "carga_horaria" not in data:
+            abort(400, message="Campos 'nome' e 'carga_horaria' são obrigatórios para PUT.")
+
+        if (
+            curso.nome != data["nome"]
+            and CursoModel.query.filter_by(nome=data["nome"]).first()
+        ):
+            abort(409, message="Já existe outro curso com esse nome.")
+
+        curso.nome = data["nome"]
+        curso.carga_horaria = data["carga_horaria"]
+
+        try:
+            db.session.add(curso)
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+            abort(500, message="Erro ao atualizar curso no banco de dados.")
+
+        return curso
+
+    @blp.arguments(CursoUpdateSchema)
+    @blp.response(200, CursoSchema)
+    def patch(self, data, curso_id):
+        curso = CursoModel.query.get(curso_id)
+        if not curso:
+            abort(404, message="Curso não encontrado.")
+
+        if "nome" in data:
+            if (
+                curso.nome != data["nome"]
+                and CursoModel.query.filter_by(nome=data["nome"]).first()
+            ):
+                abort(409, message="Já existe outro curso com esse nome.")
+            curso.nome = data["nome"]
+
+        if "carga_horaria" in data:
+            curso.carga_horaria = data["carga_horaria"]
+
+        try:
+            db.session.add(curso)
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+            abort(500, message="Erro ao atualizar curso no banco de dados.")
+
+        return curso
+
     def delete(self, curso_id):
-        """Deleta um curso"""
-        if curso_id not in cursos:
-            abort(404, message=f'Curso {curso_id} não encontrado')
-        del cursos[curso_id]
+        curso = CursoModel.query.get(curso_id)
+        if not curso:
+            abort(404, message="Curso não encontrado.")
+
+        try:
+            db.session.delete(curso)
+            db.session.commit()
+        except Exception:
+            db.session.rollback()
+            abort(500, message="Erro ao deletar curso no banco de dados.")
+
+        return {"message": "Curso deletado com sucesso."}, 200
