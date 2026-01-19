f26ae0acf082659de2a46d1b159e24e29548b12f
luccxdev <luccasxdev@gmail.com>
2026-01-19 18:55:22 -0300

    Create CursoModel for course data representation

---
+++ models/curso.py
@@ -0,0 +1,10 @@
+# models/curso.py
+from db import db
+
+
+class CursoModel(db.Model):
+    __tablename__ = "cursos"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(120), nullable=False, unique=True)
+    carga_horaria = db.Column(db.Integer, nullable=False)
