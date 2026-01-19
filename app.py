7d2e25c734c982acd476ad5e2d65e4ffd7729a2f
luccxdev <luccasxdev@gmail.com>
2026-01-19 18:58:17 -0300

    Migrar app.py para usar SQLAlchemy (Parte 3)
    Refactor app initialization into a create_app function and update configuration settings.

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
 from flask import Flask
 from flask_smorest import Api
-from resource.curso import blp as curso_blpapp = Flask(__name__)
+
+from db import db
+from resource.curso import blp as CursoBlueprint
 
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "API Gestão de Cursos"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+def create_app():
+    app = Flask(__name__)
 
-api = Api(app)
+    app.config["API_TITLE"] = "API Gestão de Cursos"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/docs"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-api.register_blueprint(curso_blp)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    api = Api(app)
+    api.register_blueprint(CursoBlueprint)
+
+    @app.route("/")
+    def index():
+        return {"message": "API de Gestão de Cursos - veja a documentação em /docs"}, 200
+
+    return app
+
+
 if __name__ == "__main__":
+    app = create_app()
+
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
