12f9ebeb6163a36707e381cd38bdeb499ac41575
luccxdev <luccasxdev@gmail.com>
2026-01-19 19:14:01 -0300

    Add professor_id foreign key to curso model
    Added foreign key relationship to Professor model.

--- models/curso.py
+++ models/curso.py
@@ -8,3 +8,6 @@ class CursoModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(120), nullable=False, unique=True)
     carga_horaria = db.Column(db.Integer, nullable=False)
+        
+    # Foreign Key para Professor (relacionamento N-1)
+    professor_id = db.Column(db.Integer, db.ForeignKey('professores.id'), nullable=False)
