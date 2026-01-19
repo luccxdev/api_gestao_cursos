c0be89411c2f0eaa95d4766caf3afd917f17f461
luccxdev <luccasxdev@gmail.com>
2026-01-19 18:59:23 -0300

    Ajustar schemas com id e CursoUpdateSchema (Parte 3)

--- schemas/curso.py
+++ schemas/curso.py
@@ -1,14 +1,13 @@
+# schemas/curso.py
 from marshmallow import Schema, fields
 
 
 class CursoSchema(Schema):
     id = fields.Int(dump_only=True)
     nome = fields.Str(required=True)
-    descricao = fields.Str()
     carga_horaria = fields.Int(required=True)
 
 
-class CursoSchemaUpdate(Schema):
+class CursoUpdateSchema(Schema):
     nome = fields.Str()
-    descricao = fields.Str()
     carga_horaria = fields.Int()
